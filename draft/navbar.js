3f94c353b4bb3a69ccad4041db4b4a02bb3f173c
Dominik Bumbacher <dominik.bumbacher@students.fhnw.com>
2024-03-09 13:04:35 +0100

    Finished nav, added homepage, added next game

--- draft/navbar.js
+++ draft/navbar.js
@@ -3,12 +3,27 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener('click', function () {
-    this.classList.toggle('active');
-    var panel = this.nextElementSibling;
+    var panel = getChildByClass(this, 'dropdown-menu');
+    var toggle = getChildByClass(this, 'accordion-toggle');
+
+    toggle.classList.toggle('active');
+
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + 'px';
     }
   });
+}
+
+function getChildByClass(elem, className) {
+  var children = elem.childNodes;
+
+  for (var i = 0; i < children.length; i++) {
+    if (children[i].classList && children[i].classList.contains(className)) {
+      return children[i];
+    }
+  }
+
+  return undefined;
 }
