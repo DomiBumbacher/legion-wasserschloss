8c7755cb9172afb1840806229077d34ec1a77ccd
Dominik Bumbacher <dominik.bumbacher@students.fhnw.com>
2024-03-23 16:14:38 +0100

    Restyled navbar button

--- draft/navbar.js
+++ draft/navbar.js
@@ -10,8 +10,12 @@ for (i = 0; i < acc.length; i++) {
 
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
+      panel.style.marginTop = null;
+      panel.style.marginBottom = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + 'px';
+      panel.style.marginTop = '1rem';
+      panel.style.marginBottom = '1rem';
     }
   });
 }
