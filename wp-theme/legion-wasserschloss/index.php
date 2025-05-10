d35b548baf8dda6371fd66de233582b71f1fb4b7
Dominik Bumbacher <git.dominik.bumbacher@gmail.com>
2025-05-10 10:53:29 +0200

    Reworked index news to not use main post loop and filter for category

--- wp-theme/legion-wasserschloss/index.php
+++ wp-theme/legion-wasserschloss/index.php
@@ -86,28 +86,41 @@
         <h2 class="fp-heading">News</h2>
 
         <div class="news">
-            <?php if ( have_posts() ) : ?>
-                <!-- Limit posts on main page to 3 (Change post_per_page to edit) -->
-                <?php query_posts('posts_per_page=3'); ?>
-                <?php while ( have_posts() ) : the_post(); ?>
-                    <!-- Check for last post in the loop and remove the line -->
-                      <article class="news-article <?php if (!has_post_thumbnail()) echo 'news-article-no-image' ?>">
-                        <a href="<?php the_permalink(); ?>">
-                            <?php the_post_thumbnail('full', ['class' => 'news-article-image']) ?>
-                            
-                            <!-- <img class="news-article-image" src="<?php echo get_bloginfo('template_url') ?>/img/demo/demo_news.jpg"> -->
-                            <div class="news-article-content <?php if (!has_post_thumbnail()) echo 'news-article-content-no-image' ?>">
-                                <p class="news-article-date"><?php echo get_the_date( 'd.m.Y' ); ?></p>
-                                <h3><?php the_title(); ?></h3>
-                                <p><?php the_excerpt(); ?></p>
-                            </div>
-                        </a>
-                    </article>
-                <?php endwhile; ?>
-            <?php else : ?> 
-                <p><?php esc_html_e( 'Es wurden keine Posts gefunden. Bitte kontaktieren Sie den Seitenadministrator.' ); ?></p>
-            <?php endif; ?>
+            <?php 
+                $argsNews = array(
+                    'numberposts'	=> 3,
+                    'category'		=> 'News'
+                );
+
+                $newsPosts = get_posts( $args );
+
+                $output = '';
+                foreach ( $my_posts as $p ){
+                    $output .= '<article class="news-article';
+                    if(has_post_thumbnail($p->ID)) $output .= ' news-article-no-image';
+                    $output .= '">';
+
+                    $output .= '<a href="' . get_permalink($p->ID). '">';
+
+                    $output .= get_the_post_thumbnail($p->ID, 'full', ['class' => 'news-article-image']);
+
+                    $output .= '<div class="news-article-content'
+                    if(has_post_thumbnail($p->ID)) $output .= 'news-article-content-no-image';
+                    $output .= '">';
+
+                    $output .= '<p class="news-article-date">' . get_the_date( 'd.m.Y' , $p->ID) . '</p>';
+                    $output .= '<h3>' . get_the_title($p->ID) . '</h3>';
+                    $output .= '<p>' .  get_the_excerpt($p->ID) . '</p>';
+
+                    $output .= '</div>';
+                    $output .= '</a>';
+                    $output .= '</article>';
+                }
+
+                echo $output;
+            ?>      
         </div>
+
     </div>
 
 </section>
